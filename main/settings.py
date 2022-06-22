c1adeef4396453f81a9927772a930a1de77d3645
Hemanth Kantubhukta <hemanth680684@gmail.com>
2022-06-22 19:40:28 +0530

    pre-final

--- main/settings.py
+++ main/settings.py
@@ -14,8 +14,6 @@ from pathlib import Path
 import os
 import mimetypes
 import django_heroku
-import dj_database_url
-from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,7 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-#STATIC_URL = 'static/'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
